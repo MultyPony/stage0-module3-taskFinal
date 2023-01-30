911aa9b40c1e4097dc776a3b3f96075c1d261130
Ivan Meshcheryakov <meshcheryakovvrn@gmail.com>
2023-01-30 11:56:22 +0400

    Module #3. Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int d1 = number / 1000;
+        int d2 = (number % 1000) / 100;
+        int d3 = ((number % 1000) % 100) / 10;
+        int d4 = number % 10;
+
+        System.out.println(d1 + d2 + d3 + d4);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int firstDigit = (number / 100);
+        int middleDigit = ((number % 100) / 10) * 10;
+        int lastDigit = (number % 10) * 100;
+
+        System.out.println(firstDigit + middleDigit + lastDigit);
     }
 }
